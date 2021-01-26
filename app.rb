8a4f4505c07237a2b94ebda093145fb2cbaa7449
ongaeshi <ongaeshi0621@gmail.com>
2021-01-26 20:50:38 +0900

    feat: Comment out require "sinatra/reloader"

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "bundler/setup"
 Bundler.require
 require 'open-uri'
 # require 'sinatra'
-require "sinatra/reloader" if development?
+# require "sinatra/reloader" if development?
 
 get '/' do
   text = get_text("https://scrapbox.io/api/pages/ongaeshi/extensive_reading/text")
